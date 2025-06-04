3bc6711c3a70a0a427f1515f669d05e3c494a78d
sabb87 <febriantisabrina06@gmail.com>
2025-06-05 01:00:18 +0700

    hapus

--- utils/generate_qr.py
+++
@@ -1,9 +0,0 @@
-import qrcode
-
-def generate_qr_code(text, output_path="qr_code.png"):
-    qr = qrcode.QRCode(version=1, box_size=10, border=4)
-    qr.add_data(text)
-    qr.make(fit=True)
-    img = qr.make_image(fill_color="black", back_color="white")
-    img.save(output_path)
-    return output_path
