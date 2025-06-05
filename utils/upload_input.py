30fc2bd94ee2a3ed6913355f59d4c42cbd9272a3
ariahma <ariahmadhudori03@gmail.com>
2025-06-05 09:50:35 +0700

    update dari ari

---
+++ utils/upload_input.py
@@ -0,0 +1,9 @@
+def upload_and_save_image(uploaded_file):
+    import os
+    if uploaded_file:
+        os.makedirs("uploads", exist_ok=True)
+        file_path = os.path.join("uploads", uploaded_file.name)
+        with open(file_path, "wb") as f:
+            f.write(uploaded_file.getbuffer())
+        return file_path
+    return None
