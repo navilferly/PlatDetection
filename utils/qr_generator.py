804fc54742177cb1c7dd187aeefed6c71d91a4fe
fajarferdiansyahkusuma <ferdiansyahfajar201@gmail.com>
2025-06-08 14:53:59 +0700

    perubahan qr

--- utils/qr_generator.py
+++ utils/qr_generator.py
@@ -1,7 +1,10 @@
-def generate_plate_qr(plate):
-    from generate_qr import generate_qr
-    from urllib.parse import urlencode
-    base_url = "http://localhost:8501/"
-    full_url = f"{base_url}?" + urlencode({"plate": plate})
-    qr_path = generate_qr(full_url)
+import qrcode
+import os
+
+def generate_plate_qr(plate, output_path='static'):
+    os.makedirs(output_path, exist_ok=True)
+    full_url = f"http://192.168.4.109/?plate={plate}"
+    img = qrcode.make(full_url)
+    qr_path = os.path.join(output_path, f'{plate}_qr.png')
+    img.save(qr_path)
     return qr_path, full_url
